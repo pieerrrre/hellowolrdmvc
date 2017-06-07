50085064028001181021ed83574a9eb08d66b776
pierre <pierre@10.151.128.102>
2017-06-07 10:00:41 +0200

    phase2

---
+++ helloworldmaven/helloworldmvc.Contract/src/main/java/fr/exia/helloworldmaven/IModel.java
@@ -0,0 +1,9 @@
+package fr.exia.helloworldmaven;
+
+public interface IModel {
+public class Model implements IModel {
+	public String getHelloWorld(){
+		return null;
+	}
+}
+}
---
+++ helloworldmaven/helloworldmvc.Contract/src/main/java/fr/exia/helloworldmaven/IView.java
@@ -0,0 +1,5 @@
+package fr.exia.helloworldmaven;
+
+public interface IView {
+
+}
